9bd9106c8dff9edb0543abaf313f54bd64bd609d
IrinaTentina <irina_tentina@mail.ru>
2015-09-28 18:28:53 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         { 
             Console.WriteLine(' ');
-            int a = 5;
+            int b = 5;
 
         }
     }
